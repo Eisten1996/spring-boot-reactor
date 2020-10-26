ea25df24d9c9c2b83b87e811d6852f24572256fa
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-10-25 19:41:19 -0500

    Creando un Flux (Observable) a partir de un List o Iterable

--- src/main/java/com/dipper/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/dipper/springboot/reactor/app/SpringBootReactorApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
 
@@ -19,7 +22,15 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Flux<String> nombres = Flux.just("Andres Guzman", "Maria Fulana", "Juan Mengano", "Pedro Fulano", "Diego Sultano", "Bruce Lee", "Bruce Willis");
+        List<String> usuariosList = new ArrayList<>();
+        usuariosList.add("Andres Guzman");
+        usuariosList.add("Maria Fulana");
+        usuariosList.add("Juan Mengano");
+        usuariosList.add("Pedro Fulano");
+        usuariosList.add("Diego Sultano");
+        usuariosList.add("Bruce Lee");
+        usuariosList.add("Bruce Willis");
+        Flux<String> nombres = Flux.fromIterable(usuariosList);/*Flux.just("Andres Guzman", "Maria Fulana", "Juan Mengano", "Pedro Fulano", "Diego Sultano", "Bruce Lee", "Bruce Willis");*/
         Flux<Usuario> usuarios = nombres.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
                 .filter(usuario -> usuario.getNombre().equalsIgnoreCase("Bruce"))
                 .doOnNext(usuario -> {
